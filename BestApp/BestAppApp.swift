4933c6f28d4e975404d42334aa42274f56c8b43a
jermaine-iris <danieljermaine97@gmail.com>
2025-06-18 11:44:34 +0100

    Initial Commit

---
+++ BestApp/BestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestAppApp.swift
+//  BestApp
+//
+//  Created by Daniel Jermaine on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
